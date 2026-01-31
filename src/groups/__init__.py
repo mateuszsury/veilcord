b10949d56fab70cb252635878ab3fc04da3a2d6d
Mateusz <mateusz.sury25@gmail.com>
2026-01-31 04:02:12 +0100

    chore(07-03): update groups __init__ with service exports
    - Export GroupService and GroupServiceCallbacks
    - Export invite functions: generate_invite_code, parse_invite_code, validate_invite_code
    - Export InviteData dataclass
    - Organize __all__ by category

--- src/groups/__init__.py
+++ src/groups/__init__.py
@@ -1,12 +1,29 @@
 """Group features module."""
 from src.groups.models import Group, GroupMember, SenderKeyState
 from src.groups.sender_keys import SenderKey, SenderKeyReceiver, EncryptedGroupMessage
+from src.groups.group_service import GroupService, GroupServiceCallbacks
+from src.groups.invite import (
+    generate_invite_code,
+    parse_invite_code,
+    validate_invite_code,
+    InviteData
+)
 
 __all__ = [
+    # Models
     "Group",
     "GroupMember",
     "SenderKeyState",
+    # Sender Keys
     "SenderKey",
     "SenderKeyReceiver",
     "EncryptedGroupMessage",
+    # Service
+    "GroupService",
+    "GroupServiceCallbacks",
+    # Invite
+    "generate_invite_code",
+    "parse_invite_code",
+    "validate_invite_code",
+    "InviteData",
 ]
